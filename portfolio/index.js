9248c6c93232927d7abc0aabce0327a544a69743
zvenrm <zvennnnnn@gmail.com>
2022-01-09 21:22:42 +0300

    feat: create adaptive and burger menu for header and hero below than 768

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,1 +1,12 @@
+const hamburger = document.querySelector('.header-burger');
+const navigation = document.querySelector('.header-navigation');
+
+function toggleMenu() {
+    hamburger.classList.toggle('navigation-open');
+    navigation.classList.toggle('navigation-open');
+}
+hamburger.addEventListener('click', toggleMenu);
+navigation.addEventListener('click', toggleMenu);
+
+
 console.log('Вёрстка валидная 10/10\nВёрстка семантическая 20/20\nВёрстка соответствует макету 48/48\nТребования к css 12/12\nИнтерактивность, реализуемая через css 20/20\nИтого 110/110');
